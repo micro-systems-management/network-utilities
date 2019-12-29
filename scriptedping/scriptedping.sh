bf3e7b76a56eea5e7ff14d05fe460099cf4b8773
4D5A <4D5A@users.noreply.github.com>
2019-12-29 17:35:24 -0500

    Renamed a directory and change an if statement.

--- scriptedping/scriptedping.sh
+++ scriptedping/scriptedping.sh
@@ -19,27 +19,26 @@
 export a=''
 export b=''
 export datetime=$(date +%Y%m%d_%H%M%S)
-if ! [[ -d ~/bin/test/$datetime ]] ; then
-	mkdir ~/bin/test/$datetime/tmp -p
-	mkdir ~/bin/test/$datetime/archive -p
-	cp -f ./scriptedping.sh ~/bin/test/
-	chmod +x ~/bin/test/scriptedping.sh
+if ! [[ -d ~/bin/scriptedping/$datetime ]] ; then
+	mkdir ~/bin/scriptedping/$datetime/tmp -p
+	mkdir ~/bin/scriptedping/$datetime/archive -p
+	cp -f ./scriptedping.sh ~/bin/scriptedping/
+	chmod +x ~/bin/scriptedping/scriptedping.sh
 else
-	cp -f ./scriptedping.sh ~/bin/test/
-	chmod +x ~/bin/test/scriptedping.sh
+	cp -f ./scriptedping.sh ~/bin/scriptedping/
+	chmod +x ~/bin/scriptedping/scriptedping.sh
 fi
-screen -d -m -S test1 bash -c 'ping $a | while read pong; do echo "$(date): $pong"; done > ~/bin/test/$datetime/tmp/box_Network-ping-$a-$datetime.log.tmp'
-screen -d -m -S test2 bash -c 'ping $b | while read pong; do echo "$(date): $pong"; done > ~/bin/test/$datetime/tmp/box_Network-ping-$b-$datetime.log.tmp'
+screen -d -m -S test1 bash -c 'ping $a | while read pong; do echo "$(date): $pong"; done > ~/bin/scriptedping/$datetime/tmp/box_Network-ping-$a-$datetime.log.tmp'
+screen -d -m -S test2 bash -c 'ping $b | while read pong; do echo "$(date): $pong"; done > ~/bin/scriptedping/$datetime/tmp/box_Network-ping-$b-$datetime.log.tmp'
 sleep 10
 screen -XS test1 quit
 screen -XS test2 quit	
-test1=$(awk -F'time=|ms' '{if($2>200)print$0}' <~/bin/test/$datetime/tmp/box_Network-ping-$a-$datetime.log.tmp)
-test2=$(awk -F'time=|ms' '{if($2>200)print$0}' <~/bin/test/$datetime/tmp/box_Network-ping-$b-$datetime.log.tmp)
+test1=$(awk -F'time=|ms' '{if($2>200)print$0}' <~/bin/scriptedping/$datetime/tmp/box_Network-ping-$a-$datetime.log.tmp)
+test2=$(awk -F'time=|ms' '{if($2>200)print$0}' <~/bin/scriptedping/$datetime/tmp/box_Network-ping-$b-$datetime.log.tmp)
 if [[ "$test1" ]] || [[ "$test2" ]] ; then
-	mv ~/bin/test/$datetime/tmp/box_Network-ping-$a-$datetime.log.tmp ~/bin/test/$datetime/archive/box_Network-ping-$a-$datetime.log
-	mv ~/bin/test/$datetime/tmp/box_Network-ping-$b-$datetime.log.tmp ~/bin/test/$datetime/archive/box_Network-ping-$b-$datetime.log
-	traceroute $a > ~/bin/test/$datetime/archive/traceroute-$a-$(date +%Y%m%d_%H%M%S).log
-	traceroute $b > ~/bin/test/$datetime/archive/traceroute-$b-$(date +%Y%m%d_%H%M%S).log
-else
-	rm -r ~/bin/test/$datetime
+	mv ~/bin/scriptedping/$datetime/tmp/box_Network-ping-$a-$datetime.log.tmp ~/bin/scriptedping/archive/$datetime/box_Network-ping-$a-$datetime.log
+	mv ~/bin/scriptedping/$datetime/tmp/box_Network-ping-$b-$datetime.log.tmp ~/bin/scriptedping/archive/$datetime//box_Network-ping-$b-$datetime.log
+	traceroute $a > ~/bin/scriptedping/archive/$datetime/traceroute-$a-$(date +%Y%m%d_%H%M%S).log
+	traceroute $b > ~/bin/scriptedping/archive/$datetime/traceroute-$b-$(date +%Y%m%d_%H%M%S).log
 fi
+rm -r ~/bin/scriptedping/$datetime
