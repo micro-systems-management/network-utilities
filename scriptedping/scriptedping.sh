d85b989858217fcff84020350df3a35a5c1fb448
4D5A <4D5A@users.noreply.github.com>
2019-12-29 03:08:49 -0500

    edit

--- scriptedping/scriptedping.sh
+++ scriptedping/scriptedping.sh
@@ -1,4 +1,21 @@
-
+#    Copyright (c) Micro Systems Management 2018, 2019. All rights reserved.
+#    
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    Licensed under the GNU-3.0-or-later license. A copy of the
+#    GNU General Public License is available in the LICENSE file
+#    located in the root of this repository. If not, see
+#    <https://www.gnu.org/licenses/>.
+#
+#!/bin/bash
 export a=''
 export b=''
 export datetime=$(date +%Y%m%d_%H%M%S)
