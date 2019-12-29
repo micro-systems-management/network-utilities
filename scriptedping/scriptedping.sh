093a0dc73942dedd119fbdb7ad57a1138fd4b3d4
4D5A <4D5A@users.noreply.github.com>
2019-12-29 03:11:26 -0500

    update

--- scriptedping/scriptedping.sh
+++ scriptedping/scriptedping.sh
@@ -1,4 +1,4 @@
-#    Copyright (c) Micro Systems Management 2018, 2019. All rights reserved.
+#    Copyright (c) Micro Systems Management 2019. All rights reserved.
 #    
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
