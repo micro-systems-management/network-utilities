443d3d5fa9198c4fec5c1f4e5ae5f19adea07907
4D5A <michael.zinn@msmctech.com>
2020-01-09 15:07:23 -0500

    added information in the copyright

--- scriptedping/scriptedping.sh
+++ scriptedping/scriptedping.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#    Copyright (c) Micro Systems Management 2019. All rights reserved.
+#    Copyright (c) Micro Systems Management 2019 - 2020. All rights reserved.
 #    
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
